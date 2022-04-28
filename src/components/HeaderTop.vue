afbbf399358010619183fcb61b68adae89769bd4
David Freitas <davidfreitas.dev@outlook.com>
2022-04-28 11:42:34 -0300

    project initial commit

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        <nav class="nav">
+            <div class="nav-title">Church Management</div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style socped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-page>
+    <header-top></header-top>
+    
+    <ion-content :fullscreen="true">
+      <div class="container">
+        <!-- Content Here -->
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonPage } from '@ionic/vue';
+import HeaderTop from '@/components/HeaderTop.vue';
+
+export default ({
+  name: 'HomePage',
+  components: { IonContent,  IonPage, HeaderTop }
+});
+</script>
+
+<style scoped>
+
+</style>
