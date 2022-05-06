560267d8c56e7316feeb7992f06f3199bb655624
David Freitas <davidfreitas.dev@outlook.com>
2022-05-06 15:46:12 -0300

    Restructuring views directory

--- src/router/index.js
+++ src/router/index.js
@@ -19,52 +19,52 @@ const routes = [
   {
     path: '/ancient',
     name: 'ancient',
-    component: () => import('../views/AncientPage.vue')
+    component: () => import('../views/ancient/AncientPage.vue')
   },
   {
     path: '/preachers-timeline',
     name: 'preachers-timeline',
-    component: () => import('../views/PreachersTimeLine.vue')
+    component: () => import('../views/ancient/PreachersTimeLine.vue')
   },
   {
     path: '/preachers-list',
     name: 'preachers-list',
-    component: () => import('../views/PreachersList.vue')
+    component: () => import('../views/ancient/PreachersList.vue')
   },
   {
     path: '/music',
     name: 'Music',
-    component: () => import('../views/MusicPage.vue')
+    component: () => import('../views/music/MusicPage.vue')
   },
   {
     path: '/singers-timeline',
     name: 'singers-timeline',
-    component: () => import('../views/SingersTimeLine.vue')
+    component: () => import('../views/music/SingersTimeLine.vue')
   },
   {
     path: '/singers-list',
     name: 'singers-list',
-    component: () => import('../views/SingersList.vue')
+    component: () => import('../views/music/SingersList.vue')
   },
   {
     path: '/reception',
     name: 'Reception',
-    component: () => import('../views/ReceptionPage.vue')
+    component: () => import('../views/reception/ReceptionPage.vue')
   },
   {
     path: '/receptionists-timeline',
     name: 'receptionists-timeline',
-    component: () => import('../views/ReceptionistsTimeLine.vue')
+    component: () => import('../views/reception/ReceptionistsTimeLine.vue')
   },
   {
     path: '/visitors-list',
     name: 'visitors-list',
-    component: () => import('../views/VisitorsList.vue')
+    component: () => import('../views/reception/VisitorsList.vue')
   },
   {
     path: '/media',
     name: 'Media',
-    component: () => import('../views/MediaPage.vue')
+    component: () => import('../views/media/MediaPage.vue')
   },
 ]
 
